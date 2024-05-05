4b24876949c2cc83c0952a350a024293ac099a93
bigalsoft informatica <bigalsoft.info@gmail.com~>
2024-05-05 10:49:46 -0300

    Sintaxe básica P2 e Comentários - Curso de Python #02

---
+++ aula02/aula02.py
@@ -0,0 +1,15 @@
+# num=1; comentario
+
+"""
+comentario de multiplas linhas
+"""
+num1=num2=res=0;
+num="fran";
+num=1.5;
+canal = "CFB Cursos"; curso = "Cursos de Python";
+print(canal + " - " +  curso);
+
+if 10 < 2:
+    print("maior")
+    print("aula02")
+print ("fim")
